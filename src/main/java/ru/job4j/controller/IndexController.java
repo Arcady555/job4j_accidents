d01e634c1a45b785ef54bb923341758985b83986
Arcady555 <parfenov7233@gmail.com>
2022-11-24 17:39:37 +0300

    Spring MVC [#6877]

--- src/main/java/ru/job4j/controller/IndexController.java
+++ src/main/java/ru/job4j/controller/IndexController.java
@@ -1,13 +1,14 @@
 package ru.job4j.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/index")
-    public String indexGet() {
+    public String indexGet(Model model) {
         return "index";
     }
 }
