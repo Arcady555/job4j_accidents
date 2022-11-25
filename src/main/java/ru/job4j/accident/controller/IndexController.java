e5eb20fdc5b88a6e9af9ada12ffb32fd06993d56
Arcady555 <parfenov7233@gmail.com>
2022-11-25 15:20:15 +0300

    IndexControl. Таблица и вид. [#2092]

---
+++ src/main/java/ru/job4j/accident/controller/IndexController.java
@@ -0,0 +1,23 @@
+package ru.job4j.accident.controller;
+
+import lombok.AllArgsConstructor;
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.accident.service.AccidentService;
+
+@Controller
+@AllArgsConstructor
+@ThreadSafe
+@RequestMapping("/accident")
+public class IndexController {
+
+    private final AccidentService accidentService;
+
+    @GetMapping("/index")
+    public String indexGet(Model model) {
+        model.addAttribute("accidents", accidentService.findAll());
+        return "index";
+    }
+}
---
+++ src/main/java/ru/job4j/accident/service/AccidentService.java
@@ -0,0 +1,20 @@
+package ru.job4j.accident.service;
+
+import lombok.AllArgsConstructor;
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Service;
+import ru.job4j.accident.model.Accident;
+import ru.job4j.accident.repository.AccidentMem;
+
+import java.util.Map;
+
+@Service
+@AllArgsConstructor
+@ThreadSafe
+public class AccidentService {
+    private final AccidentMem accidentMem;
+
+    public Map<Integer, Accident> findAll() {
+        return accidentMem.findAll();
+    }
+}
