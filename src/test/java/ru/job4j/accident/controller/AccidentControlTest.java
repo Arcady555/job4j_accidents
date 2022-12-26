4fcf9c3139787255e98ab6b1bc1495d294e55675
Arcady555 <parfenov7233@gmail.com>
2022-12-26 22:50:46 +0300

    Spring mock [#2096 #373407]

--- src/test/java/ru/job4j/accident/controller/AccidentControlTest.java
+++ src/test/java/ru/job4j/accident/controller/AccidentControlTest.java
@@ -9,15 +9,26 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.Main;
+import ru.job4j.accident.model.Accident;
+import ru.job4j.accident.service.AccidentService;
+
+import org.mockito.ArgumentCaptor;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 public class AccidentControlTest {
+
+    @MockBean
+    private AccidentService accidentService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -67,5 +78,17 @@ public class AccidentControlTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("setRule"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(post("/accidents/create")
+                        .param("name","Авария!!!"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Accident> argument = ArgumentCaptor.forClass(Accident.class);
+        verify(accidentService).create(argument.capture());
+        assertThat(argument.getValue().getName(), is("Авария!!!"));
     }
 }
