d08a158a3bcf113e98d7b1a4fca66f84e9f6d4df
Arcady555 <parfenov7233@gmail.com>
2022-12-27 20:47:09 +0300

    Spring test [#6881 #373402]-X

--- src/main/java/ru/job4j/accident/config/JdbcConfig.java
+++ src/main/java/ru/job4j/accident/config/JdbcConfig.java
@@ -15,10 +15,10 @@ import javax.sql.DataSource;
 public class JdbcConfig {
 
     @Bean
-    public DataSource ds(@Value("${driver}") String driver,
-                         @Value("${url}") String url,
-                         @Value("${username}") String username,
-                         @Value("${password}") String password) {
+    public DataSource ds(@Value("${spring.datasource.driver}") String driver,
+                         @Value("${spring.datasource.url}") String url,
+                         @Value("${spring.datasource.username}") String username,
+                         @Value("${spring.datasource.password}") String password) {
         BasicDataSource ds = new BasicDataSource();
         ds.setDriverClassName(driver);
         ds.setUrl(url);
